280d37e30cab0cc0a1023ad0d231037e1929b779
Rahul Choudhary <rahulcy7@gmail.com>
2020-07-15 12:05:46 +0530

    add navbar and useritem components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "GitHub Finder",
-    icon: "fa fa-github",
-  };
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon}> {this.props.title}</i>
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon}> {title}</i>
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "GitHub Finder",
+  icon: "fa fa-github",
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
